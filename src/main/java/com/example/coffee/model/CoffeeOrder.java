03f278b229cd68a782a53a68fccf494dde6671e7
LotaIbe <ibelota10@gmail.com>
2024-07-07 19:58:36 +0100

    starter code for coffee order

---
+++ src/main/java/com/example/coffee/model/CoffeeOrder.java
@@ -0,0 +1,6 @@
+package com.example.coffee.model;
+
+public class CoffeeOrder {
+    public CoffeeOrder(String size, String type, int quantity) {
+    }
+}
---
+++ src/main/java/com/example/coffee/service/CoffeeOrderService.java
@@ -0,0 +1,10 @@
+package com.example.coffee.service;
+
+import com.example.coffee.model.CoffeeOrder;
+
+public class CoffeeOrderService {
+
+    public CoffeeOrderService() {
+    }
+
+}
